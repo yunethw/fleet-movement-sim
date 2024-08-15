fff2181b124517b16662bd50d979313a2ce27b58
Yuneth Wijenayake <yunethcwij@gmail.com>
2024-08-15 13:36:29 +0530

    Create dataframe to store coordinates and distances

--- fleet-movement-sim.py
+++ fleet-movement-sim.py
@@ -1,40 +1,46 @@
 # 10 Trains
 # Traveling along predefined routes
-from typing import List
 import json
 import geojson
+import pandas as pd
+import numpy as np
+from typing import List
 from shapely.geometry import shape, Point, LineString
 from geopy.distance import geodesic
 
 
 class Start:
+
     def __init__(self, name, time, coordinates: tuple):
         self.name = name
         self.time = time
         self.coordinates = coordinates
+        self.index = 0
 
     def __str__(self):
-        return f'{self.name} {self.time} {self.coordinates}'
+        return f'{self.index} {self.name} {self.time} {self.coordinates}'
+
+    def setIndex(self, index):
+        self.index = index
 
 
 class Stop:
+
     def __init__(self, name, leg_duration_mins, stop_duration_mins, coordinates: tuple):
         self.name = name
         self.leg_duration_mins = leg_duration_mins
         self.stop_duration_mins = stop_duration_mins
         self.coordinates = coordinates
+        self.index = 0
 
     def __str__(self):
-        return f'{self.name} {self.coordinates}'
+        return f'{self.index} {self.name} {self.coordinates}'
+
+    def setIndex(self, index):
+        self.index = index
 
 
 class Train:
-    route: LineString
-    Vc = 0.0  # Current velocity
-    Vt = 0.0  # Target velocity
-    Vmax = 0.0  # Maximum velocity
-    A = 0.0  # Acceleration
-    Dbreak = 0.0  # Breaking distance
 
     def __init__(self, name: str, mac: str, route: str, direction: str,
                  start: Start, stops: List[Stop], max_speed, max_acceleration, breaking_distance):
@@ -44,12 +50,39 @@ class Train:
         self.route_direction = direction
         self.start = start
         self.stops = stops
-        self.Vmax = max_speed
-        self.A = max_acceleration
-        self.Dbreak = breaking_distance
+        self.v_max = max_speed
+        self.a = max_acceleration
+        self.d_break = breaking_distance
+
+        self.route = None
+        self.df = None
+        self.v_c = 0.0
+        self.v_t = 0.0
 
     def setRouteLineString(self, route: LineString):
         self.route = route
+        self.df = pd.DataFrame(route.coords, columns=['Long', 'Lat'])
+        gap = []
+        for i in range(len(route.coords)):
+            if i == 0:
+                gap.append(0.0)
+            else:
+                point1 = (route.coords[i - 1][1], route.coords[i - 1][0])
+                point2 = (route.coords[i][1], route.coords[i][0])
+                gap.append(round(geodesic(point1, point2).meters, 3))
+
+            if self.start.coordinates == route.coords[i]:
+                self.start.setIndex(i)
+            for stop in self.stops:
+                if stop.coordinates == route.coords[i]:
+                    stop.setIndex(i)
+                    break
+
+        cum_dist = np.cumsum(gap)
+        self.df['Gap'] = gap
+        self.df['CumulativeDistance'] = cum_dist
+        # print(self.df.head(10))
+        # print(self.df.tail(10))
 
     def __str__(self):
         return f'''Train: {self.name}
@@ -57,9 +90,9 @@ class Train:
         Route: {self.route_name} {self.route_direction}
         Start: {str(self.start)}
         Stops: {', '.join([str(stop) for stop in self.stops])}
-        Max Speed: {self.Vmax}
-        Max Acceleration: {self.A}
-        Breaking Distance: {self.Dbreak}
+        Max Speed: {self.v_max}
+        Max Acceleration: {self.a}
+        Breaking Distance: {self.d_break}
         '''
 
 
@@ -68,10 +101,10 @@ def main():
     for feature in railway_routes['features']:
         total_distance = 0.0
         coords = feature['geometry']['coordinates']
-        for i in range(len(coords) - 1):
-            point1 = (coords[i][1], coords[i][0])
-            point2 = (coords[i + 1][1], coords[i + 1][0])
-            total_distance += geodesic(point1, point2).kilometers
+        # for i in range(len(coords) - 1):
+        #     point1 = (coords[i][1], coords[i][0])
+        #     point2 = (coords[i + 1][1], coords[i + 1][0])
+        #     total_distance += geodesic(point1, point2).kilometers
         print(feature['properties']['name'], feature['geometry']['type'],
               len(feature['geometry']['coordinates']), 'coordinates', round(total_distance, 3), 'km')
 
@@ -79,12 +112,14 @@ def main():
     with open('fleet.json') as f:
         data = json.load(f)
         for train_data in data['collection']:
-            start = Start(train_data['start']['name'], train_data['start']['time'], train_data['start']['coordinates'])
+            start_coords = tuple(round(c, 6) for c in train_data['start']['coordinates'])
+            start = Start(train_data['start']['name'], train_data['start']['time'], start_coords)
 
             stops = []
             for stop_data in train_data['stops']:
+                stop_coords = tuple(round(c, 6) for c in stop_data['coordinates'])
                 stop = Stop(stop_data['name'], stop_data['legDurationMins'],
-                            stop_data['stopDurationMins'], stop_data['coordinates'])
+                            stop_data['stopDurationMins'], stop_coords)
                 stops.append(stop)
 
             train = Train(train_data['name'], train_data['deviceMAC'], train_data['route'],
